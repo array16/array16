b67a04420783101701788e4789ba18de354065f2
array16 <88081287+array16@users.noreply.github.com>
2022-07-20 16:08:58 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
 import pickle
 from pathlib import Path
 
-import pandas as pd  # pip install pandas openpyxl
-import plotly.express as px  # pip install plotly-express
+import pandas as pd  
 import streamlit as st  # pip install streamlit
 import streamlit_authenticator as stauth  # pip install streamlit-authenticator
 
